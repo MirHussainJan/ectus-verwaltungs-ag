bc6cd063a62a8094017c0e9d1adfd8e9bba5c5c8
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-23 23:12:15 +0500

    Logos in section 2 updated

--- src/features/home/components/Section2.jsx
+++ src/features/home/components/Section2.jsx
@@ -4,6 +4,17 @@ import logo1 from "../../../assets/images/hmlogo.png";
 import logo2 from "../../../assets/images/mediamark.png";
 import logo3 from "../../../assets/images/spotify.png";
 import logo4 from "../../../assets/images/dbbahn.png";
+import logo5 from "../../../assets/images/amazon.png";
+import logo6 from "../../../assets/images/tesla.png";
+import logo7 from "../../../assets/images/alibaba.png";
+import logo8 from "../../../assets/images/Saudi.png";
+import logo9 from "../../../assets/images/Huawei.png"
+import logo10 from "../../../assets/images/addidas.png"
+import logo11 from "../../../assets/images/dm.png";
+import logo12 from "../../../assets/images/Booking.png";
+import logo13 from "../../../assets/images/Swarovski.png";
+import logo14 from "../../../assets/images/samsung.png";
+import logo15 from "../../../assets/images/ikea.png"
 import Image from "next/image";
 
 export const Section2 = () => {
@@ -17,12 +28,12 @@ export const Section2 = () => {
         <div className="flex-1 justify-center">
           <Slider
             width="250px"
-            duration={10}
+            duration={20}
             pauseOnHover
             blurBorders={false}
             blurBorderColor="#fff"
           >
-            {[logo1, logo2, logo3, logo4].map((logo, index) => (
+            {[logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11, logo12, logo13, logo14, logo15].map((logo, index) => (
               <Slider.Slide key={index}>
                 <div className="flex justify-center items-center w-full h-[48px]">
                   <Image src={logo} alt={`logo-${index}`} height={48} />

83a02eb47d8f25fc19c6432554e639b75c46368e
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-23 23:26:43 +0500

    Section 7 completed

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { Section3 } from "@/features/home/components/Section3";
 import { Section4 } from "@/features/home/components/Section4";
 import { Section5 } from "@/features/home/components/Section5";
 import { Section6 } from "@/features/home/components/Section6";
+import { Section7 } from "@/features/home/components/Section7";
 export default function Home() {
   return (
     <div>
@@ -13,6 +14,7 @@ export default function Home() {
       <Section4 />
       <Section5 />
       <Section6 />
+      <Section7 />
     </div>
   );
 }
---
+++ src/features/home/components/Section7.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export const Section7
+ = () => {
+  return (
+    <div className="bg-[#F5F5F5] py-12 px-4 md:px-6 lg:px-[8.333vw] lg:py-[85px]">
+      <h2 className="font-bold text-[24px]/[130%] lg:text-[40px]/[130%] text-center mb-6 lg:mb-[64px] w-full">
+        Our Team
+      </h2>
+      <div className="flex flex-col md:flex-row gap-6 lg:gap-12">
+        <div className="bg-white rounded-[16px] p-5 flex-1">
+          <h3 className="font-bold text-[20px]/[150%] lg:text-[32px]/[150%] text-[#1D1E20] mb-1">
+            Mustafa Sizan
+          </h3>
+          <p className="lg:text-[20px]/[150%] text-[16px]/[150%] text-[#0171FF]">
+            Managing Director
+          </p>
+        </div>
+        <div className="bg-white rounded-[16px] p-5 flex-1">
+          <h3 className="font-bold text-[20px]/[150%] lg:text-[32px]/[150%] text-[#1D1E20] mb-1">
+            Sven Flieshardt
+          </h3>
+          <p className="lg:text-[20px]/[150%] text-[16px]/[150%] text-[#0171FF]">
+            Department Head
+          </p>
+        </div>
+        <div className="bg-white rounded-[16px] p-5 flex-1">
+          <h3 className="font-bold text-[20px]/[150%] lg:text-[32px]/[150%] text-[#1D1E20] mb-1">
+            Laura Lehmann
+          </h3>
+          <p className="lg:text-[20px]/[150%] text-[16px]/[150%] text-[#0171FF]">
+            Secretary
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

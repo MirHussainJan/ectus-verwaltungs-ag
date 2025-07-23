aef8d5c10a54a735c72cff652d91eb2f94dd9cdc
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-23 19:24:40 +0500

    Section 3 Fixes

--- src/features/home/components/Section3.jsx
+++ src/features/home/components/Section3.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import Image from "next/image";
-import imagedesk from "../../../assets/images//sec3desk.png";
-import imageipad from "../../../assets/images/sec3ipad.png";
-import imagemob from "../../../assets/images/sec3mob.png";
+import image from "../../../assets/images/sec3.png";
 
 export const Section3 = () => {
   return (
     <div className="flex flex-col-reverse lg:flex-row lg:gap-[90px] gap-8">
-      <div className="lg:w-[40vw] w-full">
-        <div className="hidden lg:block">
-          <Image className="w-full" src={imagedesk} alt="" />
-        </div>
-        <div className="hidden md:block lg:hidden">
-          <Image className="w-full" src={imageipad} alt="" />
-        </div>
-        <div className="block md:hidden">
-          <Image className="w-full" src={imagemob} alt="" />
-        </div>
+      <div className="lg:w-[45.972vw] relative py-12">
+        <div className="absolute bg-[#F3F4F7] top-0 left-0 w-full h-[85%] -z-10"></div>
+        <div className="absolute bg-[#F3F4F7] bottom-0 left-0 w-[85%] h-full -z-10"></div>
+        <Image
+          className="w-[92.07vw] lg:w-[41.944vw] h-full object-cover rounded-r-[36px]"
+          src={image}
+          alt=""
+        />
       </div>
-      <div className="flex flex-1 flex-col justify-center lg:pr-[8.333vw]">
-        <h2 className="font-bold text-[40px]/[130%] lg:pr-[6.042vw] mb-4 uppercase">
+      <div className="flex flex-1 flex-col justify-center px-6  lg:pr-[8.333vw] py-10">
+        <h2 className="font-bold text-[24px]/[130%] lg:text-[40px]/[130%] lg:pr-[6.042vw] mb-4 uppercase">
           About BlackRock and SK Black Rock Financial
         </h2>
         <div className="flex flex-col gap-4">
-          <p className='text-[18px]/[28px] text-[#626262]'>
+          <p className="text-[16px]/[140%] lg:text-[18px]/[28px] text-[#626262]">
             BlackRock is one of the most recognized names in the financial
             world, known for its global leadership in asset management.
             Established in 1988 in New York by Larry Fink and seven partners,
@@ -33,7 +29,7 @@ export const Section3 = () => {
             risk solutions, and advisory services for institutional and private
             clients worldwide.
           </p>
-          <p className='text-[18px]/[28px] text-[#626262]'>
+          <p className="text-[18px]/[28px] text-[#626262]">
             A key milestone in BlackRock’s growth was the 2009 acquisition of
             Barclays Global Investors, which positioned the firm as the largest
             asset manager in the world. Today, BlackRock manages a diverse
@@ -41,7 +37,7 @@ export const Section3 = () => {
             exchange-traded funds (ETFs) through its iShares brand, serving
             millions of clients globally.
           </p>
-          <p className='text-[18px]/[28px] text-[#626262]'>
+          <p className="text-[18px]/[28px] text-[#626262]">
             In line with this global expansion, 2023 saw the establishment of SK
             Black Rock Financial GmbH in Monheim am Rhein, Germany, under the
             management of Mustafa Sizan. While SK Black Rock Financial operates

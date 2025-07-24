7974ab8d053fe6a33a6f28573e2a52b013a26045
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-24 20:18:20 +0500

    Tab 4 completed

--- src/features/Privay/Tab4.jsx
+++ src/features/Privay/Tab4.jsx
@@ -2,6 +2,306 @@ import React from 'react'
 
 export const Tab4 = () => {
   return (
-    <div>Tab4</div>
-  )
+    <div className="lg:w-[45.833vw]">
+      <p className="text-[16px]/[24px] text-black">
+        <strong>BlackRock Cookie Notice</strong>
+        <br />
+        <br />
+        Last revised: 11 June 2024
+        <br />
+        <br />
+        <strong>Introduction</strong>
+        <br />
+        <br />
+        This Cookie Notice applies to the use by BlackRock of cookies and
+        similar technologies (collectively “cookies”) on the BlackRock Websites
+        (“our Websites”). The purpose of this Cookie Notice is to explain what
+        cookies are, how they work, the cookies we use and how you can control
+        which ones we use when you visit our Websites. It also describes the
+        purposes for which we process Personal Information (“PI”) collected
+        through our use of cookies and your rights. It also sets out
+        individuals’ rights in relation to the processing of their PI by
+        cookies.
+        <br />
+        <br />
+        “Our Websites” mean any publicly available, BlackRock-domain, operated
+        by BlackRock or by a third party on behalf of BlackRock.
+        <br />
+        <br />
+        “BlackRock”, “we”, “us” or “our” means BlackRock, Inc., and each of the
+        direct or indirect subsidiaries of BlackRock, Inc., (the “BlackRock
+        Group”).
+        <br />
+        <br />
+        References to “you” or “your” relate to individuals who visit our
+        Websites.
+        <br />
+        <br />
+        This Cookie Notice should be read in conjunction with&nbsp;
+        <a href="#" className="underline text-blue-600">
+          BlackRock’s Privacy Notice
+        </a>
+        .
+        <br />
+        <br />
+        This Cookie Notice does not apply to any domains that we do not own or
+        control, or to any third-party domains where BlackRock advertisements
+        are displayed.
+      </p>
+      <p className="text-[16px]/[24px] text-black">
+        <br />
+        <br />
+        <strong>What is a cookie?</strong>
+        <br />
+        <br />
+        A cookie is a small text file which is downloaded to your device or
+        browser when you visit a website. Cookies are then sent back to the
+        originating website on each subsequent visit, or to another website that
+        recognises that cookie, in order to develop a record of the user’s
+        online activity. Cookies may be delivered by the website a user visits,
+        or by third parties. First party cookies are set by BlackRock and belong
+        to the website domain that appears in the browser address bar.
+        <br />
+        <br />
+        <strong>Types of cookies:</strong>
+        <br />
+        <br />
+      </p>
+      <ol className="list-decimal pl-6 space-y-4 text-[16px]/[24px] text-black">
+        <li>
+          <strong>Necessary cookies</strong>
+          <br />
+          Necessary cookies help keep our websites secure while enabling network
+          management, site functionality and accessibility. You may disable
+          necessary cookies in your browser settings, but that may affect how
+          the website functions for you.
+        </li>
+        <li>
+          <strong>Preference cookies</strong>
+          <br />
+          Preference cookies enable us to store certain user selections or
+          preferences and allows for a more tailored website experience.
+        </li>
+        <li>
+          <strong>Statistical Analytics</strong>
+          <br />
+          Measure the performance of our website and collect aggregate level
+          information on how you use our site, for us to provide you with the
+          best possible digital experience. No user level based behavioural data
+          will be collected as part of this method. If you are visiting a
+          European site, these are only active if you click “Accept all”. On
+          Non-European sites, you may opt out of these separately under “Manage
+          cookies”.
+        </li>
+        <li>
+          <strong>Marketing/Tracking/Profiling cookies</strong>
+          <br />
+          Marketing/Tracking/Profiling cookies are usually placed by advertising
+          networks and are used to deliver relevant content or measure campaign
+          performance. Information about website usage and visits may be shared
+          with other organizations such as advertisers.
+        </li>
+        <li>
+          <strong>Conversational cookies</strong>
+          <br />
+          Conversational cookies help tie conversations and interactions to a
+          specific visitor on our site, when you are interacting with our
+          chatbot. These cookies must be accepted in order to interact with the
+          chatbot on our websites.
+        </li>
+      </ol>
+      <p className="text-[16px]/[24px] text-black">
+        <br />
+        <br />
+        <strong>Consent and updating your cookie preferences</strong>
+        <br />
+        <br />
+        On our European Websites, we will not set any cookies other than those
+        that are Strictly Necessary without your consent.
+        <br />
+        <br />
+        On our Websites outside Europe, by clicking “Required only”, Statistical
+        Analytics and Strictly Necessary cookies will be set as they do not
+        collect any Personal Information. You may opt out of Statistical
+        Analytics separately under “Manage cookies”.
+        <br />
+        <br />
+        If you are navigating between our European Websites and our Websites
+        outside Europe, your first choice will determine the setting of
+        Statistical Analytics; you can always change your preference under
+        “Manage cookies”.
+        <br />
+        <br />
+        When you first visit our Website and on subsequent visits you will have
+        the option to choose and then amend your other cookie preferences by
+        clicking the Manage Cookies button which will appear on each page.
+        <br />
+        <br />
+        Across all sites, you will only be prompted again if we update the list
+        of cookies we utilize or if this affects your browsing.
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>Cookies used on our Websites</strong>
+        <br />
+        <br />
+        If you’d like more information on the specific cookies currently used,
+        their purpose and how long they last, please{" "}
+        <a href="#" className="underline text-blue-600">
+          click here
+        </a>
+        . (only available in English)
+        <br />
+        <br />
+        "Session" Cookies are temporary and will expire at the end of a browser
+        session; that is, when you leave a website. These allow our website to
+        recognize you and carry information as you navigate between pages during
+        a single browser session and allow you to use the website efficiently.
+        <br />
+        <br />
+        Other cookies have a defined expiry period as set out in the link above.
+        Cookies will not be listed if they have expired, however some expired
+        cookies may still be listed in your browser cookies settings even though
+        they are not current on our website.
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>Non-cookie tracking technologies</strong>
+        <br />
+        <br />
+        We may also use web beacons including conversion pixels, which are small
+        codes located on a particular web page which are triggered when someone
+        visits a page resulting in an increase in the conversion count, or other
+        technologies for similar purposes. We may include these on our sites, in
+        our marketing e-mail messages or newsletters, or on affiliated websites,
+        including client platforms and products, to determine whether messages
+        have been opened and links clicked on.
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>How to block all cookies and what this will mean</strong>
+        <br />
+        <br />
+        If you want to block all cookies, you may be able to do this through the
+        settings in your browser for each browser you use and each device you
+        use. If you block all cookies, you may find certain features on our
+        website will not function properly and we may not be able to respond to
+        your request or provide you with the best service.
+        <br />
+        <br />
+        Please remember, if you turn off cookies using the settings in your
+        browser, this will apply to all websites, not just BlackRock.
+      </p>
+      <p className="text-[16px]/[24px] text-black">
+        <br />
+        <br />
+        <strong>Further information</strong>
+        <br />
+        <br />
+        You can find more information about cookies on the following websites:
+        <br />
+        <a
+          href="https://www.allaboutcookies.org"
+          target="_blank"
+          className="underline text-blue-600"
+        >
+          www.allaboutcookies.org
+        </a>
+        <br />
+        <a
+          href="https://www.youronlinechoices.eu"
+          target="_blank"
+          className="underline text-blue-600"
+        >
+          www.youronlinechoices.eu
+        </a>
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>Contacting us</strong>
+        <br />
+        <br />
+        The Global Head of Privacy and Data Protection oversees compliance with
+        privacy and data protection law and regulation at BlackRock. If you wish
+        to exercise any of your rights, or have questions concerning this Cookie
+        Notice, please contact:
+        <br />
+        <br />
+        The Global Head of Privacy and Data Protection
+        <br />
+        BlackRock
+        <br />
+        12 Throgmorton Avenue
+        <br />
+        London
+        <br />
+        EC2N&nbsp;2DL
+        <br />
+        Email:&nbsp;
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          GroupPrivacy@BlackRock.com
+        </a>
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>Complaints</strong>
+        <br />
+        <br />
+        If you have any concerns or complaints about the way your PI is
+        processed, please contact the Global Head of Privacy and Data Protection
+        at&nbsp;
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          GroupPrivacy@BlackRock.com
+        </a>
+        . You also have a right to complain to a data protection or other
+        competent authority with jurisdiction over privacy and data protection
+        law in the country you live or work, or in the country where you believe
+        an issue in relation to the processing of your Personal Information has
+        arisen. Please contact&nbsp;
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          GroupPrivacy@BlackRock.com
+        </a>
+        &nbsp;for further details.
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>BlackRock Privacy Notice</strong>
+        <br />
+        <br />
+        Please see our separate&nbsp;
+        <a href="#" className="underline text-blue-600">
+          BlackRock Privacy Notice
+        </a>
+        .
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>Recruitment Privacy Notice</strong>
+        <br />
+        <br />
+        Please see our separate&nbsp;
+        <a href="#" className="underline text-blue-600">
+          Recruitment Privacy Notice
+        </a>
+        .
+      </p>
+
+      <p className="text-[16px]/[24px] text-black mt-6">
+        <strong>Changes to this Cookie Notice</strong>
+        <br />
+        <br />
+        We may modify or amend this Cookie Notice from time to time. You are
+        advised to visit our website regularly to check for any amendments.
+      </p>
+    </div>
+  );
 }

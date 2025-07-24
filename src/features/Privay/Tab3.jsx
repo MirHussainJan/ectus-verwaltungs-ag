08b119319016771b5db4c2c08852add79304d6c1
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-24 19:43:51 +0500

    Tab 3 Content added

--- src/features/Privay/Tab3.jsx
+++ src/features/Privay/Tab3.jsx
@@ -2,6 +2,335 @@ import React from 'react'
 
 export const Tab3 = () => {
   return (
-    <div>Tab3</div>
-  )
+    <div className="lg:w-[45.833vw]">
+      <p className="text-[16px]/[24px] text-black">
+        <strong>BlackRock Alumni Privacy Notice</strong>
+        <br />
+        <br />
+        Last revised: 1 June 2024
+        <br />
+        <br />
+        Welcome to the BlackRock Alumni Website (“Alumni Website”). BlackRock is
+        committed to processing personal information (“PI”) in line with all
+        applicable privacy and data protection laws. “BlackRock”, “we”, “us” or
+        “our” means BlackRock, Inc., and each of the direct or indirect
+        subsidiaries of BlackRock, Inc.
+        <br />
+        <br />
+        <strong>PI we collect about you</strong>
+        <br />
+        <br />
+        PI is information relating to an individual, which can be used either
+        alone or with other sources of information to identify that individual.
+        PI does not include information where the identity of the individual has
+        been removed and is therefore non-attributable.
+        <br />
+        <br />
+        To join BlackRock’s alumni network, you will be asked to submit an
+        application on our Alumni Website. Your participation in our network is
+        entirely your choice. As part of your application, we may collect the
+        following PI that can be categorized as follows (the PI listed for each
+        category are non-exhaustive examples):
+      </p>
+      <ul className="list-disc pl-6 space-y-4 my-4 text-[16px]/[24px] text-black">
+        <li>
+          <strong>Identification data</strong>
+          <br />
+          Full name, gender, photograph.
+        </li>
+        <li>
+          <strong>Personal Contact data</strong>
+          <br />
+          Email address, current residential location.
+        </li>
+        <li>
+          <strong>Professional Information data</strong>
+          <br />
+          Current employment status, management title, line of business,
+          employment experience, education, board memberships, BlackRock email
+          address, information relating to your time at BlackRock, including
+          management level, city last worked, line of business, title, and time
+          of joining and leaving BlackRock.
+        </li>
+      </ul>
+      <p className="text-[16px]/[24px] text-black">
+        Unless we otherwise indicate that the provision of specific PI is
+        optional, any PI we request from you is necessary to progress your
+        application to become a member of the BlackRock alumni network.
+        <br />
+        <br />
+        <strong>The purposes and legal basis for processing your PI</strong>
+        <br />
+        <br />
+        The below table sets out the purposes and bases for which we process PI.
+      </p>
+      <div className="grid grid-cols-3 gap-6 text-[16px]/[24px] text-black my-6">
+        <div className="font-bold">Processing Purpose</div>
+        <div className="font-bold">Category of PI</div>
+        <div className="font-bold">Basis of Processing</div>
+      </div>
+      <hr className="my-6" />
+      <div className="grid grid-cols-3 gap-6 text-[16px]/[24px] text-black my-6">
+        <div>
+          <strong>
+            {" "}
+            To process your application to enable you to join our alumni network
+          </strong>
+        </div>
+        <div>
+          · Identification data
+          <br />
+          <br />
+          · Personal contact data
+          <br />
+          <br />· Professional information data
+        </div>
+        <div>
+          · Performance of a contract
+          <br />
+          <br />
+          · Consent
+          <br />
+          <br />· Legitimate interests of BlackRock: ensure that applications to
+          join our alumni network are made only by BlackRock alumni
+        </div>
+      </div>
+      <hr className="my-6" />
+      <div className="grid grid-cols-3 gap-6 text-[16px]/[24px] text-black my-6">
+        <div className="font-bold">
+          To send communications that include BlackRock news, thought
+          leadership, career opportunities, alumni stories, and invitations to
+          alumni events
+        </div>
+        <div>· Personal contact data</div>
+        <div>
+          · Consent
+          <br />
+          <br />· Legitimate interests of BlackRock: ensure that our alumni are
+          made aware of content and opportunities relevant to the alumni network
+        </div>
+      </div>
+      <hr className="my-6" />
+      <p className="text-[16px]/[24px] text-black">
+        <strong>To whom we may disclose your PI</strong>
+        <br />
+        <br />
+        In connection with one or more of the purposes outlined in the section
+        ‘Purpose and Legal basis for processing your PI’ above, we may disclose
+        PI in any jurisdiction to:
+      </p>
+
+      <ul className="list-disc pl-6 space-y-4 my-4 text-[16px]/[24px] text-black">
+        <li>Other members of BlackRock;</li>
+        <li>
+          Any service providers who provide services to us in relation to the
+          alumni network;
+        </li>
+        <li>
+          Any person in connection with litigation or other legal proceedings,
+          to obtain legal advice or for establishing, exercising or defending
+          legal rights;
+        </li>
+        <li>
+          Any person to whom disclosure is allowed or required by law and/or
+          regulation;
+        </li>
+        <li>
+          Any court, tribunal, regulatory authority or Government entity; and
+        </li>
+        <li>
+          Third parties to whom BlackRock may transfer its rights and/or
+          obligations under any agreement, including but not limited to a
+          potential merger or acquisition of all or part of BlackRock’s business
+          or assets.
+        </li>
+      </ul>
+
+      <p className="text-[16px]/[24px] text-black">
+        <strong>Your rights</strong>
+        <br />
+        <br />
+        In some countries, subject to certain exemptions, you may have the
+        following rights:
+      </p>
+
+      <ul className="list-disc pl-6 space-y-4 my-4 text-[16px]/[24px] text-black">
+        <li>
+          <strong>Access</strong>
+          <br />
+          The PI processed in relation to you is accessible to you in your
+          profile on the Alumni Website.
+        </li>
+        <li>
+          <strong>Correction</strong>
+          <br />
+          You may amend your PI directly in your profile on the Alumni Website.
+        </li>
+        <li>
+          <strong>Erasure</strong>
+          <br />
+          If you wish to delete your BlackRock Alumni Website account, you may
+          email us at{" "}
+          <a
+            href="mailto:alumni@blackrock.com"
+            className="underline text-blue-600"
+          >
+            alumni@blackrock.com
+          </a>{" "}
+          to request deletion.
+        </li>
+      </ul>
+      <p className="text-[16px]/[24px] text-black">
+        Additionally, in some countries, subject to certain exemptions, you have
+        rights including, but not limited to the right to object to processing,
+        restriction, and portability. If you wish to exercise any of these
+        rights, please contact
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          {" "}
+          GroupPrivacy@BlackRock.com
+        </a>
+        .
+        <br />
+        <br />
+        You will not be disadvantaged in any way by exercising your rights in
+        relation to the processing of your PI.
+        <br />
+        <br />
+        <strong>How long your PI is retained for</strong>
+        <br />
+        <br />
+        We will process your PI for as long as is necessary to fulfil the
+        purpose for which it was collected or to comply with legal, regulatory,
+        accounting, reporting, internal policy requirements or for the
+        establishment or defence of legal claims.
+        <br />
+        <br />
+        <strong>How your PI is protected</strong>
+        <br />
+        <br />
+        BlackRock implements technical and organizational measures to
+        appropriately protect PI against unauthorized, accidental, or unlawful
+        destruction, loss, alteration, unauthorized disclosure of, or access to
+        PI transmitted, stored, or otherwise processed.
+        <br />
+        <br />
+        <strong>Transfers of PI</strong>
+        <br />
+        <br />
+        We are a global organization and in the course of running our business,
+        we may transfer PI to a location outside of the country where you
+        reside, including BlackRock processing centers in the USA, Hungary,
+        India and Singapore. Although the country to which PI may be transferred
+        may not have the same level of privacy and data protection laws, we
+        apply the same level of security and organizational controls to the
+        processing of PI wherever it is processed. We require by contract that
+        our service providers processing PI on our behalf comply with
+        BlackRock’s criteria for PI processing.
+        <br />
+        <br />
+        If we transfer PI out of the EEA, we ensure a similar level of
+        protection for your PI by ensuring the country is considered by the EU
+        Commission to provide an adequate level of protection or by putting in
+        place contractual clauses the EU Commission considers to provide the
+        same level of protection.
+        <br />
+        <br />
+        <strong>
+          Alumni communications and exercising your right to opt-out
+        </strong>
+        <br />
+        <br />
+        We will not send you communications about BlackRock news, thought
+        leadership, career opportunities, alumni stories, and invitations to
+        alumni events if you have informed us that you do not wish to receive
+        communications. All electronic communications we send to you will have
+        an unsubscribe option.
+        <br />
+        <br />
+        <strong>Third-party marketing/sale of PI</strong>
+        <br />
+        <br />
+        We do not share or sell your PI to third parties for the third party to
+        use for their own marketing or other purposes.
+        <br />
+        <br />
+        <strong>Cookies</strong>
+        <br />
+        <br />
+        Please see our separate Cookie Notice.
+        <br />
+        <br />
+        <strong>Contacting us</strong>
+        <br />
+        <br />
+        The Global Head of Privacy and Data Protection oversees compliance with
+        privacy and data protection at BlackRock. If you wish to exercise any of
+        your rights, or have questions concerning this notice, please contact:
+        <br />
+        The Global Head of Privacy and Data Protection
+        <br />
+        BlackRock
+        <br />
+        12 Throgmorton Avenue
+        <br />
+        London
+        <br />
+        EC2N 2DL
+        <br />
+        Email:{" "}
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          GroupPrivacy@BlackRock.com
+        </a>
+        <br />
+        If you are a California resident, you may also call us on{" "}
+        <a href="tel:+18553710019" className="underline text-blue-600">
+          +1 855 371 0019
+        </a>
+        .
+        <br />
+        <br />
+        <strong>Complaints</strong>
+        <br />
+        <br />
+        If you have any concerns or complaints about the way your PI is
+        processed, please contact the Global Head of Privacy and Data Protection
+        at
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          {" "}
+          GroupPrivacy@BlackRock.com
+        </a>
+        . You also have a right to complain to a data protection or other
+        competent authority with jurisdiction over privacy and data protection
+        law in the country you live or work, or in the country where you believe
+        an issue in relation to the processing of your PI has arisen. Please
+        contact
+        <a
+          href="mailto:GroupPrivacy@BlackRock.com"
+          className="underline text-blue-600"
+        >
+          {" "}
+          GroupPrivacy@BlackRock.com
+        </a>{" "}
+        for further details.
+        <br />
+        <br />
+        <strong>Changes to this Privacy Notice</strong>
+        <br />
+        <br />
+        We may modify or amend this Privacy Notice from time to time and you are
+        advised to visit our Alumni Website regularly to check for any
+        amendments. Any material changes will be communicated to you through an
+        appropriate channel, depending on how we normally communicate with you.
+      </p>
+    </div>
+  );
 }

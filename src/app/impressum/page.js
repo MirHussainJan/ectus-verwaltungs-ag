529be0656065ae797fbab000c9d4a15c79d9f9e8
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-25 20:31:02 +0500

    impressum translation

--- src/app/impressum/page.js
+++ src/app/impressum/page.js
@@ -10,21 +10,23 @@ const Impressum = () => {
             <Navbar mode="light" />
             <div className='lg:px-[8.333vw] min-h-[calc(100dvh-763px] md:min-h-[calc(100dvh-655px)] lg:min-h-[calc(100dvh-486px)] px-4 lg:pt-[42px] lg:pb-[80px] pt-[28px] pb-[48px]'>
                 <Link className='flex items-center gap-2 font-bold text-[#484848] text-[20px]/[150%]' href="/">
-                    <BackArrow /> Home</Link>
+                    <BackArrow /> Startseite
+                </Link>
                 <div className='flex flex-col gap-4 lg:mt-8 mt-6 w-[240px]'>
                     <h2 className='font-bold text-[32px]/[140%] lg:text-[40px]/[52px]%]'>Impressum</h2>
-                    <p className='text-[18px]/[28px] text-[#626262]'>SK Black Rock Financial GmbH
-                        Sandrstraße 104
+                    <p className='text-[18px]/[28px] text-[#626262]'>
+                        SK Black Rock Financial GmbH
+                        Sandstraße 104
                         40789 Monheim a. Rhein
                         <br /><br />
-
-                        Geschäftsführer : Mustafa Sizan
-                        Hauptsitz Monheim
+                        Geschäftsführer: Mustafa Sizan
+                        Hauptsitz: Monheim
                         Amtsgericht Düsseldorf
                         HRB 102003
                         DER1 101.HRB102003
                         <br /><br />
-                        E-Mail (info@sk-blackrock)</p>
+                        E-Mail: info@sk-blackrock
+                    </p>
                 </div>
             </div>
             <Footer />

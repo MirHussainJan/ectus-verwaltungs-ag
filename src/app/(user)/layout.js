e046d6489d0aae85db31e05ad375c50668620bc6
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-08-05 01:12:50 +0500

    User Routes Protected

--- src/app/(user)/layout.js
+++ src/app/(user)/layout.js
@@ -1,6 +1,21 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import NavbarPortal from "@/features/common/NavbarPortal";
-
+import { useCheckUserAuthStatus } from "@/hooks/auth";
+import LoadingBackdrop from "@/features/common/LoadingBackdrop";
 export default function StaticLayout({ children }) {
+  const router = useRouter();
+  const { isPending, isError, isSuccess } = useCheckUserAuthStatus();
+  useEffect(() => {
+    if (!isPending && isError) {
+      router.replace("/login");
+    }
+  }, [isPending, isError, router]);
+  if (isPending || (!isError && !isSuccess)) {
+    return <LoadingBackdrop />;
+  }
+  if (isError) return null;
   return (
     <>
       <NavbarPortal />
@@ -8,6 +23,5 @@ export default function StaticLayout({ children }) {
         {children}
       </div>
     </>
-
   );
 }
